3791f8a61087d310562bfc9ba34e63a88f1772c0
liaowei <l.w.kampfer@gmail.com>
2020-05-27 13:40:53 +0800

    修复bug

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ hljs.registerLanguage('javascript', javascript);
 
 let index = 0;
 
-const { Suite, platform } = window.Benchmark;
+const { Suite, platform, formatNumber } = window.Benchmark;
 
 export default class EmBenchmark {
 
@@ -24,21 +24,25 @@ export default class EmBenchmark {
         this._handleSuiteStart = this._handleSuiteStart.bind(this);
         this._handleSuiteCycle = this._handleSuiteCycle.bind(this);
         this._handleSuiteComplete = this._handleSuiteComplete.bind(this);
+        this._handleSuiteAbort = this._handleSuiteAbort.bind(this);
         this._handleBenchmarkStart = this._handleBenchmarkStart.bind(this);
         this._handleBenchmarkCycle = this._handleBenchmarkCycle.bind(this);
         this._handleBenchmarkComplete = this._handleBenchmarkComplete.bind(this);
+        this._handleBenchmarkAbort = this._handleBenchmarkAbort.bind(this);
 
         this._suite = new Suite(name, {
             onCycle: this._handleSuiteCycle,
             onComplete: this._handleSuiteComplete,
             onStart: this._handleSuiteStart,
+            onAbort: this._handleSuiteAbort,
         });
 
         benchmarks.forEach(benchmark => 
             this._suite.add(benchmark.name, benchmark.fn, {
                 onStart: this._handleBenchmarkStart,
                 onCycle: this._handleBenchmarkCycle,
-                onComplete: this._handleBenchmarkComplete
+                onComplete: this._handleBenchmarkComplete,
+                onAbort: this._handleBenchmarkAbort,
             })
         );
 
@@ -49,33 +53,102 @@ export default class EmBenchmark {
     }
 
     _handleSuiteStart() {
-        console.log('suite start');
+        this._showStopBtn();
+        for(let item of this._benchmarkList) {
+            let td = item.querySelector('td:last-child');
+            td.className = '';
+            td.innerHTML = 'pending…';
+        }
     }
 
-    _handleSuiteCycle(event) {
-        console.log('suite cycle', String(event.target));
+    _handleSuiteCycle() {
+        console.log('suite cycle');
     }
 
     _handleSuiteComplete() {
-        console.log('suite complete');
+
+        let suite = this._suite;
+
+        if (suite.aborted) return;
+
+        this._statusWrapper.innerHTML = 'Done. Ready to run again.';
+        this._showRunBtn();
+
+        let fastestBenchmark = null,
+            slowestBenchmark = null;
+
+        suite.forEach(benchmark => {
+
+            if (!fastestBenchmark || benchmark.hz > fastestBenchmark.hz) fastestBenchmark = benchmark;
+            if (!slowestBenchmark || benchmark.hz < slowestBenchmark.hz) slowestBenchmark = benchmark;
+
+        });
+
+        suite.forEach(benchmark => this._renderResult(benchmark, fastestBenchmark, slowestBenchmark));
+
+    }
+
+    _handleSuiteAbort() {
+        for(let item of this._benchmarkList) {
+            let td = item.querySelector('td:last-child');
+            td.className = '';
+            td.innerHTML = 'Ready';
+        }
+
+        this._statusWrapper.innerHTML = 'Ready to run again.';
+        this._showRunBtn();
     }
 
     _handleBenchmarkStart() {
-        console.log('benchmark start');
+        this._showStopBtn();
     }
 
     _handleBenchmarkCycle(event) {
-        console.log('benchmark cycle', String(event.target));
+        let benchmark = event.target,
+            name = benchmark.name,
+            hz = benchmark.hz,
+            size = benchmark.stats.sample.length;
+
+        // direct × 61,481,828 (49 samples)
+        this._statusWrapper.innerHTML = `${name} x ${formatNumber(hz.toFixed(hz < 100 ? 2 : 0))} (${size} samples)`;
     }
 
-    _handleBenchmarkComplete() {
-        console.log('benchmark complete');
+    _handleBenchmarkComplete(event) {
+
+        let benchmark = event.target,
+            benchmarkTd = this._root.querySelector(`#benchmark-${benchmark.id}>td:last-child`);
+
+        if (benchmark.aborted) return;
+
+        if (this._suite.running) {
+            benchmarkTd.innerHTML = 'completed';
+        } else {
+            this._renderResult(benchmark);
+        }
+
+        this._showRunBtn();
+    }
+
+    _handleBenchmarkAbort(event) {
+        let benchmark = event.target,
+            td = this._root.querySelector(`#benchmark-${benchmark.id}>td:last-child`);
+
+        td.innerHTML = 'Ready';
+
+        this._statusWrapper.innerHTML = 'Ready to run again.';
+        this._showRunBtn();
     }
 
     _handleClick(e) {
         switch(e.target.dataset.role) {
             case 'run-button':
-                this.run({ 'async': true });
+                this.run();
+                break;
+            case 'stop-button':
+                this.stop();
+                break;
+            case 'benchmark-trigger':
+                this.toggleBenchMark(e.target.dataset.id);
                 break;
         }
     }
@@ -96,8 +169,8 @@ export default class EmBenchmark {
                     trimLen = m[m.length - 1].length - 1;
 
                 return `
-                    <tr>
-                        <td title="Click to run this test again.">${benchmark.name}</td>
+                    <tr id="benchmark-${benchmark.id}">
+                        <td title="Click to run this test again." data-role="benchmark-trigger" data-id="${benchmark.id}">${benchmark.name}</td>
                         <td>
                             <pre><code class="js">${this._fixIndent(benchmark.fn.toString(), trimLen)}</code></pre>
                         </td>
@@ -111,7 +184,10 @@ export default class EmBenchmark {
             <div class="embedded-benchmark">
                 <div class="header">
                     <div class="status">Ready to run.</div>
-                    <div class="controls"><button data-role="run-button">Run tests</button></div>
+                    <div class="controls">
+                        <button data-role="run-button" class="run-btn">Run tests</button>
+                        <button data-role="stop-button" class="stop-btn">Stop running</button>
+                    </div>
                 </div>
                 <table>
                     <caption>Testing in ${platformStr}</caption>
@@ -132,19 +208,68 @@ export default class EmBenchmark {
             hljs.highlightBlock(block);
         });
 
+        this._runBtn = root.querySelector('[data-role="run-button"]');
+        this._stopBtn = root.querySelector('[data-role="stop-button"]');
+        this._statusWrapper = root.querySelector('.status');
+        this._benchmarkList = root.querySelectorAll('table>tbody>tr');
+
     }
 
-    add(...args) {
-        this._suite.add(...args);
+    _renderResult(benchmark, fastest, slowest) {
+
+        let benchmarkTd = this._root.querySelector(`#benchmark-${benchmark.id}>td:last-child`),
+            innerHTML = `${formatNumber(Math.round(benchmark.hz))}<small>±${benchmark.stats.rme.toFixed(2)}%</small>`;
+
+        benchmarkTd.className = '';
+
+        if ((!fastest && !slowest) || benchmark.id === fastest.id) {
+            innerHTML += '<small>fastest</small>';
+            benchmarkTd.classList.add('fastest')
+        } else {
+            if (benchmark.id === slowest.id) benchmarkTd.classList.add('slowest')
+
+            let rate = (fastest.hz - benchmark.hz) / fastest.hz;
+            innerHTML += `<small>${ Math.round(rate * 100) }% slower</small>`;
+        }
+
+        benchmarkTd.innerHTML = innerHTML;
+
+    }
+
+    _showRunBtn() {
+        this._stopBtn.style.display = 'none';
+        this._runBtn.style.display = 'unset';
+    }
+
+    _showStopBtn() {
+        this._stopBtn.style.display = 'unset';
+        this._runBtn.style.display = 'none';
+    }
+
+    run() {
+        this._suite.run({ 'async': true });
         return this;
     }
 
-    on() {
-        return this;
+    toggleBenchMark(id) {
+        this._suite.forEach(function(benchmark) {
+            if (benchmark.id == id) {
+                if (benchmark.running) {
+                    benchmark.abort();
+                } else {
+                    benchmark.run({ 'async': true });
+                }
+            }
+        });
     }
 
-    run(...args) {
-        this._suite.run(...args);
+    stop() {
+        let suite = this._suite;
+        if (suite.running) {
+            suite.abort();
+        } else {
+            suite.forEach(benchmark => benchmark.running && benchmark.abort());
+        }
         return this;
     }
 
